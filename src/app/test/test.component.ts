382d84ebf254be1053b6911c4f6230b2fdc4ffe3
Aravindan12 <aravind@sparkouttech.com>
2022-10-23 16:06:36 +0530

    property bindind

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,12 +10,16 @@ import { Component, OnInit } from '@angular/core';
               <h2>{{name.length}}</h2>
               <h2>{{name.toUpperCase()}}</h2>
               <h2>{{greetUser()}}</h2>
-              <h2>{{siteUrl}}</h2>`,
+              <h2>{{siteUrl}}</h2>
+              <input type="text" value="angular" [id] = "myId">
+              <input type="text" value="react" bind-disabled = "isDisabled">`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   public name = "aravind";
+  public myId = "test";
+  public isDisabled = true; //property to bind in html
   public siteUrl = window.location.href; //these kind of js methods can't be used in the template. we can use it like this way only
 
   constructor() { }
