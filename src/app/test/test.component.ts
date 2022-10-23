f068fa62cee792d3cc54ce7e4def9e8c2c56a720
Aravindan12 <aravind@sparkouttech.com>
2022-10-23 15:45:30 +0530

    interpolation-binding data from class

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `<h2>
+              welcome {{name}}                 
+              </h2>
+              <h2>{{2+2}}</h2>
+              <h2>{{"welcome " + name}}</h2>
+              <h2>{{name.length}}</h2>
+              <h2>{{name.toUpperCase()}}</h2>
+              <h2>{{greetUser()}}</h2>
+              <h2>{{siteUrl}}</h2>`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public name = "aravind";
+  public siteUrl = window.location.href; //these kind of js methods can't be used in the template. we can use it like this way only
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
   }
 
 }
